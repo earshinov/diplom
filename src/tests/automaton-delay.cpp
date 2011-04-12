91ebb47715b88eef95a7923b88f9fcde414b15de
Eugene Arshinov <earshinov@gmail.com>
2011-04-12 23:02:00 +0400

    delay: Больше тестов для алгоритма определения задержки автомата. Тестируем на всех примерах автоматов, которые у нас уже есть.

--- src/tests/automaton-delay.cpp
+++ src/tests/automaton-delay.cpp
@@ -5,6 +5,42 @@
 
 BOOST_AUTO_TEST_SUITE(TestAutomatonDelay)
 
+BOOST_AUTO_TEST_CASE(testEmpty)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createEmpty());
+	BOOST_CHECK_EQUAL(ret.automatonDelay, 0);
+}
+
+BOOST_AUTO_TEST_CASE(testSingleStateNoTransitions)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createSingleStateNoTransitions());
+	BOOST_CHECK_EQUAL(ret.delayByState[0], 0);
+	BOOST_CHECK_EQUAL(ret.automatonDelay, 0);
+}
+
+BOOST_AUTO_TEST_CASE(testSingleStateSingleTransition)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createSingleStateSingleTransition());
+	BOOST_CHECK_EQUAL(ret.delayByState[0], 0);
+	BOOST_CHECK_EQUAL(ret.automatonDelay, 0);
+}
+
+BOOST_AUTO_TEST_CASE(testTwoStatesCycle)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createTwoStatesCycle());
+	BOOST_CHECK_EQUAL(ret.delayByState[0], 0);
+	BOOST_CHECK_EQUAL(ret.delayByState[1], 0);
+	BOOST_CHECK_EQUAL(ret.automatonDelay, 0);
+}
+
+BOOST_AUTO_TEST_CASE(testTwoStatesWithProhibition)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createTwoStatesWithProhibition());
+	BOOST_CHECK_EQUAL(ret.delayByState[0], 0);
+	BOOST_CHECK_EQUAL(ret.delayByState[1], 0);
+	BOOST_CHECK_EQUAL(ret.automatonDelay, 0);
+}
+
 BOOST_AUTO_TEST_CASE(testOriginalExample)
 {
 	auto ret = AutomatonDelay::findAutomatonDelay(createOriginalExample());
@@ -12,6 +48,16 @@ BOOST_AUTO_TEST_CASE(testOriginalExample)
 	BOOST_CHECK_EQUAL(ret.delayByState[1], 0);
 	BOOST_CHECK_EQUAL(ret.delayByState[2], 0);
 	BOOST_CHECK_EQUAL(ret.delayByState[3], AUTOMATON_DELAY_INF);
+	BOOST_CHECK_EQUAL(ret.automatonDelay, AUTOMATON_DELAY_INF);
+}
+
+BOOST_AUTO_TEST_CASE(testProhibitionExample)
+{
+	auto ret = AutomatonDelay::findAutomatonDelay(createProhibitionExample());
+	BOOST_CHECK_EQUAL(ret.delayByState[0], AUTOMATON_DELAY_INF);
+	BOOST_CHECK_EQUAL(ret.delayByState[1], 0);
+	BOOST_CHECK_EQUAL(ret.delayByState[2], AUTOMATON_DELAY_INF);
+	BOOST_CHECK_EQUAL(ret.delayByState[3], 0);
 	BOOST_CHECK_EQUAL(ret.automatonDelay, AUTOMATON_DELAY_INF);
 }
 
